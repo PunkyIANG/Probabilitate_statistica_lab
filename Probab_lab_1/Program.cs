e77b8a50b06cbd3ace331ac7fceaf5b22eaad8f2
PunkyIANG <tsoorckanukris@gmail.com>
2020-12-05 19:33:10 +0200

    Initial commit

---
+++ Probab_lab_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Probab_lab_1
+{
+    class Program
+    {
+        private static int totalCalculations = 1000000;
+        private static int totalHits = 0;
+        private static ThreadLocal<Random> _tlRng;
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Dati numarul de calcule (n): ");
+            totalCalculations = int.Parse(Console.ReadLine());
+
+            _tlRng = new ThreadLocal<Random>(() => new Random());
+            
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Parallel.For(0, totalCalculations, i =>
+            {
+                CalcPosition();
+            });
+
+            stopwatch.Stop();
+            
+            Console.WriteLine("P = " + (double)totalHits/totalCalculations);
+            Console.WriteLine("Calculated in " + stopwatch.Elapsed);
+        }
+
+        private static void CalcPosition()
+        {
+            double firstAngle = _tlRng.Value.NextDouble() * 2 * Math.PI;
+            double secondAngle = _tlRng.Value.NextDouble() * 2 * Math.PI;
+
+            if ((firstAngle <= Math.PI / 2 && secondAngle <= Math.PI / 2) 
+                || (firstAngle >= 3 * Math.PI / 2 && secondAngle >= Math.PI && secondAngle <= 3 * Math.PI / 2))
+            {
+                Interlocked.Increment(ref totalHits);
+            }
+        }
+    }
+}
---
+++ Probab_lab_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Probab_lab_2
+{
+    class Program
+    {
+        private static int _totalCalculations = 1000000;
+        private static ThreadLocal<Random> _tlRng;
+        private static ThreadLocal<double> _tlFunctionSum;
+        private static double _functionSum = 0;
+        private static double _integralResult = 0;
+        
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Dati numarul de calcule (n): ");
+            _totalCalculations = int.Parse(Console.ReadLine());
+            
+            _tlRng = new ThreadLocal<Random>(() => new Random());
+            _tlFunctionSum = new ThreadLocal<double>(() => 0, true);
+            
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Parallel.For(0, _totalCalculations, i =>
+            {
+                FunctionAsync();
+            });
+            
+            foreach (var result in _tlFunctionSum.Values)
+            {
+                _functionSum += result;
+            }
+
+            stopwatch.Stop();
+
+            _integralResult = (0.5 * 0.5) * _functionSum / _totalCalculations;
+
+
+            Console.WriteLine("I = " + _integralResult);
+            Console.WriteLine("Calculated in " + stopwatch.Elapsed);
+        }
+        
+        private static void FunctionAsync()
+        {
+            double x = _tlRng.Value.NextDouble() * 0.5;
+            double y = _tlRng.Value.NextDouble() * 0.5;
+
+            if (x + y <= 0.5 && y > x)
+            {
+                _tlFunctionSum.Value += (2 - Math.Log(1 + x * x * y)) / (2 * x * y * y + 1);
+            }
+        }
+    }
+}
